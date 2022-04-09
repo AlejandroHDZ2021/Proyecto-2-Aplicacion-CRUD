d59dac4d4b8db343cbab53e70bb284eb565717e1
AlejandroHDZ2021 <alexhernandez.pro98@gmail.com>
2022-04-09 14:15:01 -0500

    index modificado

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ let currid=0;
    })
 
 document.querySelector('#guardar').addEventListener('click',function(){
-    const evento = new Array();
+    const ev = new Array();
   
     const nombre= document.getElementById('nombre').value;
     const categoria= document.getElementById('categoria').value ;
     const fecha= document.getElementById('fecha').value;
     
-    evento[0]=nombre;
-    evento[1]=categoria;
-    evento[2]=fecha;
+    ev[0]=nombre;
+    ev[1]=categoria;
+    ev[2]=fecha;
 
-    guardar(evento);
+    guardar(ev);
     limpiar();      
  })
 
-function guardar(evento) {
+function guardar(ev) {
     let n=0;
     let objeto = {};
 
-    if (evento[0]!=="" && evento[1] !== "" && evento[2]!==""){
-      n=neventos();
+    if (ev[0]!=="" && ev[1] !== "" && ev[2]!==""){
+      n=evts();
       
       if (currid==0){
         if (n==0){
@@ -44,7 +44,7 @@ function guardar(evento) {
         n=currid;
       }
 
-      objeto.datos = evento;
+      objeto.datos = ev;
       window.localStorage.setItem(n,JSON.stringify(objeto))
  
       currid=0;
@@ -52,7 +52,7 @@ function guardar(evento) {
     }
 }
 
-function neventos() {
+function evts() {
     let n=0;
     
     n=Number(window.localStorage.getItem(0))
@@ -62,8 +62,8 @@ function neventos() {
 function editar(ev){
   let id = ev.target.getAttribute('data-id')
 
-  evento = window.localStorage.getItem(id)
-  let data = JSON.parse(evento)
+  ev = window.localStorage.getItem(id)
+  let data = JSON.parse(ev)
   
   document.getElementById('nombre').value = data.datos[0];
   document.getElementById('categoria').value = data.datos[1];
@@ -83,11 +83,11 @@ function eliminar(ev){
 }
 
 
-function muestrarenglon(evento, id) {
+function fila(ev, id) {
     let renglon = '<tr>'+
-                    '<td>'+ evento.datos[0] + '</td>' +
-                    '<td>'+ evento.datos[1] + '</td>' +
-                    '<td>'+ evento.datos[2] + '</td>' +
+                    '<td>'+ ev.datos[0] + '</td>' +
+                    '<td>'+ ev.datos[1] + '</td>' +
+                    '<td>'+ ev.datos[2] + '</td>' +
                     '<td>  <a class="editar btn btn-info" type="submit" value="" data-id=' + id + '>Editar</a></td>' +
                     '<td>  <a class="eliminar btn btn-danger" type="submit" value="" data-id=' + id + '>Eliminar</a></td>' +
                   '</tr>';
@@ -103,16 +103,16 @@ tabla += '<th scope="col">Eliminar</th>';
 
 
 const container = document.querySelector('section[class="section"]')
-let evento = '';
-n=neventos();
+let ev = '';
+n=evts();
 if (n!==0){
   for (let i = 1; i <= n; i++) {
-      evento = window.localStorage.getItem(i)
-      let data = JSON.parse(evento)
+      ev = window.localStorage.getItem(i)
+      let data = JSON.parse(ev)
       console.log(data)
 
       if (data !== null){
-        tabla += muestrarenglon(data, i);   
+        tabla += fila(data, i);   
       }
     }
 }
